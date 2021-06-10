16b41625a1e551cc9d2a817cee4c339183f311bf
Mateo Galic <mateogalic112@gmail.com>
2021-06-10 15:10:57 +0200

    Adding stripe

--- api/order/controllers/order.js
+++ api/order/controllers/order.js
@@ -2,6 +2,9 @@
 
 const { sanitizeEntity } = require("strapi-utils");
 
+const fromDecimalToInt = (number) => parseInt(number * 100);
+
+const stripe = require("stripe")(process.env.STRIPE_SK);
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
  * to customize this controller
@@ -48,5 +51,61 @@ module.exports = {
       customer: user.id,
     });
     return sanitizeEntity(entity, { model: strapi.models.order });
+  },
+
+  /**
+   * Creates order and checkout session that belongs to logged in user.
+   *
+   * @return {Object}
+   */
+  async create(ctx) {
+    const { product } = ctx.request.body;
+
+    if (!product) {
+      return ctx.throw(400, "No product");
+    }
+
+    const realProduct = await strapi.services.product.findOne({
+      id: product.id,
+    });
+
+    if (!realProduct) {
+      return ctx.throw(404, "No product finb");
+    }
+
+    const { user } = ctx.state;
+
+    const BASE_URL = ctx.request.headers.origin || "http://localhost:3000";
+
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price_data: {
+            currency: "usd",
+            product_data: {
+              name: realProduct.name,
+            },
+            unit_amount: fromDecimalToInt(realProduct.price),
+          },
+          quantity: 1,
+        },
+      ],
+      customer_email: user.email, //Automatically added by Magic Link
+      mode: "payment",
+      success_url: `${BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}`,
+      cancel_url: BASE_URL,
+    });
+
+    // Create order
+    const newOrder = await strapi.services.order.create({
+      customer: user.id,
+      products: [realProduct.id],
+      total: realProduct.price,
+      status: "unpaid",
+      checkout_session: session.id,
+    });
+
+    return { id: session.id };
   },
 };
